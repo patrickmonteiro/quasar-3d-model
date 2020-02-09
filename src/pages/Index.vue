749d8dcd48671f5d8a333434181a128d5a9b1bb9
Patrick Monteiro <eng.patrickmonteiro@gmail.com>
2020-02-09 14:07:41 -0300

    First Commit :tada:

---
+++ src/pages/Index.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-page class="container">
+    <div class="orientation-portrait text-center">
+      <p class="text-h6">Por favor, vire o celular para visualizar</p>
+      <p class="text-h6">Please turn the phone over to view.</p>
+      <q-icon name="smartphone" size="10rem" class="rotate-90"/>
+    </div>
+    <div class="orientation-landscape">
+      <model-stl v-if="show" src="statics/Modelo_Inferior.stl" :backgroundColor="'black'"></model-stl>
+      <q-btn label="Show" icon="visibility" @click="show = true" class="full-width q-mt-lg" color="primary" />
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { ModelStl } from 'vue-3d-model'
+export default {
+  name: 'PageIndex',
+  components: {
+    ModelStl
+  },
+  data () {
+    return {
+      show: false
+    }
+  }
+}
+</script>
